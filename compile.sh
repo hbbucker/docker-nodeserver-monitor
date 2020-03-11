6ec6598262b7d766b393caa874597fbe7f3dc30a
Hugo Bucker <hbbucker@gmail.com>
2020-03-11 01:02:00 -0400

    ajuste no script de build do container

--- compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export GRAALVM_HOME=/home/bucker/Documentos/Aplicativos/graalvm/graalvm-latest
-JAVA_HOME=${GRAALVM_HOME}
-
-./mvnw package
-docker build -f src/main/docker/Dockerfile.jvm -t quarkus/docker-monitor-jvm .
